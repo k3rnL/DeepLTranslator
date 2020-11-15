418630f7ec151bd58e8017e7926939598bac474d
Erwan Daniel <erwan.daniel@epitech.eu>
2020-11-15 19:30:19 +0100

    Fix too long URI error

--- deepl.py
+++ deepl.py
@@ -26,6 +26,17 @@ def translate(text: str, to_lang: str, from_lang: str = None, formality: str = '
     return r.json()['translations'][0]['text']
 
 
+def max_texts(texts: List[str], max_chars: int):
+    count = 0
+    index = 0
+    for text in texts:
+        if count + len(text) > max_chars:
+            return index
+        count += len(text)
+        index += 1
+    return index
+
+
 def translate_batch(texts: List[str], to_lang: str, from_lang: str = None, formality: str = 'default', verbose: bool = True) -> List[str]:
 
     # clone the list
@@ -38,15 +49,17 @@ def translate_batch(texts: List[str], to_lang: str, from_lang: str = None, forma
         if verbose:
             print('\rTranslation... ' + str(len(translated_texts) / total_texts * 100) + '%', end='')
 
+        texts_to_take = max_texts(texts, 1500)
+
         data = {
             'auth_key': '9dad9d05-7f40-21f5-5eac-0e37f4ef6389',
             'target_lang': to_lang,
             'from_lang': from_lang,
             'formality': formality,
-            'text': texts[:50]
+            'text': texts[:texts_to_take]
         }
 
-        texts = texts[50:]
+        texts = texts[texts_to_take:]
 
         r = requests.get(API_URL, data)
 
